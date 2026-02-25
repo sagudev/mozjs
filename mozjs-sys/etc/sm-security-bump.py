2e44eb37d8f1533044ce9a93f438b9d058833e8e
sagudev <16504129+sagudev@users.noreply.github.com>
2026-02-25 09:22:35 +0100

    .
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- mozjs-sys/etc/sm-security-bump.py
+++ mozjs-sys/etc/sm-security-bump.py
@@ -26,18 +26,17 @@ except subprocess.CalledProcessError:
 
 download_from_taskcluster(changeset)
 
-subprocess.check_call(
-    [
-        "gh",
-        "attestation",
-        "verify",
-        "mozjs.tar.xz",
-        "allFunctions.txt.gz",
-        "gcFunctions.txt.gz",
-        "--repo",
-        "servo/mozjs",
-    ]
-)
+for file in ["mozjs.tar.xz", "allFunctions.txt.gz", "gcFunctions.txt.gz"]:
+    subprocess.check_call(
+        [
+            "gh",
+            "attestation",
+            "verify",
+            file,
+            "--repo",
+            "servo/mozjs",
+        ]
+    )
 
 subprocess.check_call(
     [
